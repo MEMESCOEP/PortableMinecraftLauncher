c4531c198fd6ef88cd706770010e35226246db17
Andrew Maney <andrewmaney05@gmail.com>
2022-03-24 09:51:31 -0400

    Add files via upload

---
+++ Compile_MC_Launcher.bat
@@ -0,0 +1,15 @@
+@echo off
+color 0a
+pyinstaller --onefile ./MC_Launcher.py
+if %ERRORLEVEL% NEQ 0 goto error
+
+move .\dist\MC_Launcher.exe .\
+rmdir /Q dist /s
+rmdir /Q build /s
+rmdir /Q __pycache__ /s
+goto EOF
+
+:error
+pause
+
+:EOF
